9e757842bfb06fa9c3706eb497811f25061c8a57
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-02-24 02:39:29 +0600

    item create

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        Hello
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-from/>
+        </div>
+        <list-view />
     </div>
 </template>
 
 <script>
+import addItemFrom from './addItemFrom'
+import listView from './listView'
+
 export default {
-    
+    components : {
+        addItemFrom,
+        listView
+    }
 }
 </script>
+
+<style scoped>
+    .todoListContainer{
+        width: 350px;
+        margin: auto;
+    }
+
+    .heading{
+        background: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title{
+        text-align: center;
+    }
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Khan
+    </div>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
